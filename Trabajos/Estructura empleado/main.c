8e72ce6ffea4d790b38ecf09d5d12f97d23ea571
blehalklos <klosowskiadan@gmail.com>
2018-04-16 12:09:35 -0300

    =)

--- Trabajos/Estructura empleado/main.c
+++ Trabajos/Estructura empleado/main.c
@@ -13,16 +13,17 @@ typedef struct
 }eEmpleado;
 
 void mostrarEmpleado(eEmpleado);
+void mostrarEmpleados(eEmpleado vec[], int tam);
 
 int main()
 {
-    eEmpleado unEmpleado = {"Juan",1234, 50000.50, 'm'};
+    eEmpleado plantel[] = {{"Juan",1234, 50000.50, 'm'},{"Ana", 2222, 1, 'f'},{"Luis", 3333, 100000, 'm'}};
 /*
     unEmpleado.legajo = 1234;
     strcpy(unEmpleado.nombre, "Juan");
     unEmpleado.sueldo = 50000.50;
     unEmpleado.sexo = 'm';
-*/
+
     printf("Ingrese nombre: ");
     gets(unEmpleado.nombre);
     printf("Ingrese legajo: ");
@@ -32,15 +33,25 @@ int main()
     fflush(stdin);
     printf("Ingrese sexo: ");
     scanf("%c", &unEmpleado.sexo);
-
-    mostrarEmpleado(unEmpleado);
+*/
+    mostrarEmpleados(plantel, 3);
 
     return 0;
 }
+    void mostrarEmpleados(eEmpleado vec[], int tam)
+    {
+        for (int i=0; i< tam; i++)
+        {
+            mostrarEmpleados (vec[i]);
+        }
+
     void mostrarEmpleado(eEmpleado emp)
     {
     printf("nombre: %s \n", emp.nombre);
     printf("legajo: %d \n", emp.legajo);
     printf("sueldo: %.2f \n", emp.sueldo);
     printf("sexo: %c \n", emp.sexo);
+    }
+
+
     }
